e89f1e1c9b622850dd0aa172cc4aa4f4b3b766bf
Bas de Bruijn <s.f.debruijn@gmail.com>
2015-12-05 17:12:50 +0100

    add game start code to NEAT

--- neat/src/main/scala/NEAT.scala
+++ neat/src/main/scala/NEAT.scala
@@ -1,6 +1,23 @@
+import agent.{PlayerInputProvider, BallFollower}
+import server.{GameProperties, PhysicsProperties, GameState, GameLoop}
+import ui.SwingUI
+
 /**
   * Created by bas on 5-12-15.
   */
-class NEAT {
+object NEAT {
+
+  def main(args: Array[String]) = {
+    val gameProps = new GameProperties()
+    val physProps = new PhysicsProperties()
+    while (true) {
+      val lInput = new BallFollower(gameProps.playerRadius / 2)
+      val rInput = new BallFollower(gameProps.playerRadius / 2)
+      val s = new GameState(gameProps, physProps, lInput, rInput)
+      //         run(s);
+      val ui = new SwingUI(gameProps)
+      new GameLoop(60, ui, s).run()
+    }
+  }
 
 }

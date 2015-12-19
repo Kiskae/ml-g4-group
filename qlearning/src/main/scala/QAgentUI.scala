3f0b454c9566c215722eb63a21c0a74ddd06635e
Michael LeKander <mlekande@calpoly.edu>
2015-12-19 03:31:40 +0100

    Flat indexing into qTable, qTable serialization, and seperate UI executable. Works best when running from the jar: gradle fatJar && java -cp build/libs/qlearning-all.jar QAgentTrainer

---
+++ qlearning/src/main/scala/QAgentUI.scala
@@ -0,0 +1,25 @@
+import server._
+import agent.{PlayerInput => Input}
+import server.{GameStateInterface => State}
+import ui.SwingUI
+import java.io.File
+
+object QAgentUI extends App {
+  val gameProps:GameProperties = new GameProperties()
+  val physProps:PhysicsProperties = new PhysicsProperties()
+
+  val qAgent = new TableQLearner(gameProps,physProps,randChance=0,alpha=0)
+  val ui:SwingUI = new SwingUI(gameProps)
+  val s:GameState = new GameState(gameProps, physProps, qAgent, qAgent)
+
+  ui.init(s)
+  while (true) {
+    qAgent.loadFromFile(new File("QTable.tsv"))
+    while (!s.isFinished()) {
+      s.step()
+      ui.display(s)
+      Thread.sleep(5)
+    }
+    s.reset()
+  }
+}

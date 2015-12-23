490ad92b6b3421a0e35ac06496b8991cac603256
Bas de Bruijn <s.f.debruijn@gmail.com>
2015-12-23 20:27:26 +0100

    fix innovation numbers

--- neat/src/main/scala/data/InnovationPool.scala
+++ neat/src/main/scala/data/InnovationPool.scala
@@ -8,16 +8,16 @@ import scala.collection.mutable.Map
   * Created by bas on 22-12-15.
   */
 object InnovationPool {
-  var pool: Map[(Neuron, Neuron), Int] = Map[(Neuron, Neuron), Int]()
+  var pool: Map[(Int, Int), Int] = Map[(Int, Int), Int]()
   var nextInnovationNumber = 0
 
   def getInnovationNumber(neurons : (Neuron, Neuron)): Int = {
-    val innovationNumber = pool.get(neurons)
-println("innovation number = " + innovationNumber)
+    val innovationNumber = pool.get((neurons._1.label, neurons._2.label))
+
     if(innovationNumber != None){
       innovationNumber.get
     }else{
-      pool.put(neurons, nextInnovationNumber)
+      pool.put((neurons._1.label, neurons._2.label), nextInnovationNumber)
       val temp = nextInnovationNumber
       nextInnovationNumber += 1
       temp

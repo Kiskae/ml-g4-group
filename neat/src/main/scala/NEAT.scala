c66934e7c3e4d3bbcea719225c7bc6e075b126fe
Bas de Bruijn <s.f.debruijn@gmail.com>
2015-12-28 20:34:06 +0100

    remove obsolete ENeuralNetwork

--- neat/src/main/scala/NEAT.scala
+++ neat/src/main/scala/NEAT.scala
@@ -49,10 +49,6 @@ object NEAT extends Logging {
       val rInputNetwork = new NEATInputProvider(bestNetwork)
 
       println(s"Starting generation $i/$generationCount.")
-//      for (neuralNetwork <- generation.networks) {
-//        val lInput = new NEATInputProvider(neuralNetwork)
-//        neuralNetwork.score = evaluate(lInput, rInputNetwork, false)
-//      }
 
       generation.networks.par.foreach( neuralNetwork => {
         val lInput = new NEATInputProvider(neuralNetwork)

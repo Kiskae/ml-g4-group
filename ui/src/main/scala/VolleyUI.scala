2a734f4cba62be5d230e86c16a6739447b25cbe7
Michael LeKander <mlekande@calpoly.edu>
2016-01-08 00:14:28 +0100

    UI with agent selection.

---
+++ ui/src/main/scala/VolleyUI.scala
@@ -0,0 +1,28 @@
+import server._
+import agent._
+import ui.SwingUI
+
+object VolleyUI extends App {
+  def getAgent(agentType:String, arg:String) = {
+    agentType.toLowerCase match {
+      case "qagent" => new carlo.QTableInputProvider(new carlo.QTable(gameProps,physProps,Some(arg)))
+      case "neat" => new neat.NEATInputProvider(misc.Persistent.ReadObjectFromFile[neural.NeuralNetwork](arg))
+      case "human" => new KeyboardInputProvider(ui, arg(0), arg(2), arg(1))
+      case "ballfollower" => new BallFollower((gameProps.playerRadius/arg.toDouble).toLong)
+    }
+  }
+
+  require(args.length == 4)
+
+  val gameProps:GameProperties = new GameProperties()
+  val physProps:PhysicsProperties = new PhysicsProperties()
+  val ui = new SwingUI(gameProps)
+
+  val lAgent = getAgent(args(0),args(1))
+  val rAgent = getAgent(args(2),args(3))
+
+  val game = new GameState(gameProps, physProps, lAgent, rAgent);
+
+  new GameLoop(60, ui, game).run()
+
+}  
